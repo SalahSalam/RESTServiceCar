63c947836150d8204a614cdf7f9862fc24ff92ff
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2024-11-11 13:46:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// use https (http secure).
+// http (non-secure) will make the app complain about mixed content when running the app from Azure
+const baseUrl = "http://localhost:5218/api/cars"
+
+Vue.createApp({
+    data() {
+        return {
+            cars: [],
+            vendorToGetBy: "",
+            idToGetBy: -1,
+            singleCar: null,
+            deleteId: 0,
+            deleteMessage: "",
+            addData: { model: "", vendor: "", price: 0 },
+            addMessage: "",
+            updateData: { id: 0, model: "", vendor: "", price: 0 },
+            updateMessage: ""
+        }
+    },
+    methods: {
+        getAllCars() {
+            this.getCars(baseUrl)
+        },
+        getByVendor(vendor) { // filter cars by vendor
+            const url = baseUrl + "?vendor=" + vendor
+            this.getCars(url)
+        },
+        async getCars(url) { // helper method: getAllCars + getByVendor are very similar
+            try {
+                const response = await axios.get(url)
+                this.cars = await response.data
+            } catch (ex) {
+                alert(ex.message) // https://www.w3schools.com/js/js_popup.asp
+            }
+        },
+        async getById(id) {
+            const url = baseUrl + "/" + id
+            try {
+                const response = await axios.get(url)
+                this.singleCar = await response.data
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteCar(deleteId) {
+            const url = baseUrl + "/" + deleteId
+            try {
+                response = await axios.delete(url)
+                this.deleteMessage = response.status + " " + response.statusText
+                this.getAllCars()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async addCar() {
+            try {
+                response = await axios.post(baseUrl, this.addData)
+                this.addMessage = "response " + response.status + " " + response.statusText
+                this.getAllCars()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        },
+        async updateCar() {
+            const url = baseUrl + "/" + this.updateData.id
+            try {
+                response = await axios.put(url, this.updateData)
+                this.updateMessage = "response " + response.status + " " + response.statusText
+                this.getAllCars()
+            } catch (ex) {
+                alert(ex.message)
+            }
+        }
+    }
+}).mount("#app")
